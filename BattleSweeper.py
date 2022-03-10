9c1b23224c2a6243205c95a3b2e94065e2e7b873
sora628496 <takumikamio28@gmail.com>
2022-03-11 01:51:24 +0900

    ファイルアップロード

---
+++ BattleSweeper.py
@@ -0,0 +1,354 @@
+import random
+from src.Screen import Screen, Log
+
+# ====================================================================================================================
+CLOSE = 0
+OPEN = 1
+LOCK = 2
+EXPLODED = -1
+
+ROW = 10
+COLUMN = 10
+
+BOMB_COUNT = 20
+
+LEFT = 0
+RIGHT = 1
+
+EXPLODE_PENALTY = 30
+MISS_PENALTY = 5
+
+
+def out_of_field(x, y):
+    return x < 0 or ROW <= x or y < 0 or COLUMN <= y
+
+
+# ====================================================================================================================
+
+class Cell:
+    x: int
+    y: int
+    state: int
+    bomb: bool
+    count: int
+    player_num: int
+
+    field: list
+
+    def __init__(self, x, y, field):
+        self.x = x
+        self.y = y
+        self.state = CLOSE
+        self.bomb = False
+        self.count = 0
+        self.player_num = 0
+
+        self.field = field
+
+    def get_state(self):
+        return self.state
+
+    def set_state(self, state):
+        self.state = state
+
+    def set_player_num(self, player_num):
+        self.player_num = player_num
+
+    def is_bomb(self):
+        return self.bomb
+
+    def set_bomb(self):
+        self.bomb = True
+
+    def set_count(self, count):
+        self.count = count
+
+    def left_click(self, player_num):
+        score = 0
+        if self.state == CLOSE:
+            if self.bomb:
+                self.state = EXPLODED
+                self.player_num = player_num
+                self.draw()
+                main.explode()
+            else:
+                score = self.open(player_num)
+                # print(score)
+        elif self.state == OPEN:
+            score = self.number_open(player_num)
+            if score == -1:
+                return
+        else:
+            return
+        main.calc_score(score, player_num)
+        main.change_turn()
+
+    def right_click(self, player_num):
+        self.lock(player_num)
+
+    # マス開け関数(閉じたマスに対して呼ばれる前提)
+    def open(self, player_num):
+        self.state = OPEN
+        self.player_num = player_num
+        self.draw()
+        score = 1
+        # 0開け
+        if self.count == 0:
+            for b in range(-1, 2):
+                for a in range(-1, 2):
+                    if out_of_field(self.x + a, self.y + b): continue
+                    if self.field[self.y + b][self.x + a].get_state() == CLOSE:
+                        score += self.field[self.y + b][self.x + a].open(player_num)
+        return score
+
+    # 数字開け
+    def number_open(self, player_num):
+        score = 0
+        lock_count = 0
+        for b in range(-1, 2):
+            for a in range(-1, 2):
+                if out_of_field(self.x + a, self.y + b): continue
+                state = self.field[self.y + b][self.x + a].get_state()
+                if state == LOCK or state == EXPLODED:
+                    lock_count += 1
+        if self.count != lock_count:
+            return -1
+        for b in range(-1, 2):
+            for a in range(-1, 2):
+                if out_of_field(self.x + a, self.y + b): continue
+                if self.field[self.y + b][self.x + a].get_state() == CLOSE:
+                    cell = self.field[self.y + b][self.x + a]
+                    if cell.is_bomb():
+                        cell.set_state(EXPLODED)
+                        cell.set_player_num(player_num)
+                        cell.draw()
+                        main.explode()
+                    else:
+                        score += cell.open(player_num)
+        return score
+
+    # ロック関数
+    def lock(self, player_num):
+        if self.state == CLOSE:
+            self.state = LOCK
+            self.player_num = player_num
+            self.draw()
+            main.lock()
+            main.calc_score(1, player_num)
+        elif self.state == LOCK:
+            if self.player_num == player_num:
+                self.state = CLOSE
+                self.draw()
+                main.unlock()
+                main.calc_score(-1, player_num)
+        elif self.state == OPEN:
+            self.number_lock(player_num)
+
+    # 数字ロック
+    def number_lock(self, player_num):
+        close_count = 0
+        for b in range(-1, 2):
+            for a in range(-1, 2):
+                if out_of_field(self.x + a, self.y + b): continue
+                state = self.field[self.y + b][self.x + a].get_state()
+                if state == CLOSE or state == LOCK or state == EXPLODED:
+                    close_count += 1
+        if self.count != close_count: return
+        for b in range(-1, 2):
+            for a in range(-1, 2):
+                if out_of_field(self.x + a, self.y + b): continue
+                if self.field[self.y + b][self.x + a].get_state() == CLOSE:
+                    self.field[self.y + b][self.x + a].lock(player_num)
+
+    def check_lock_miss(self):
+        return self.state == LOCK and not self.bomb
+
+    def draw(self):
+        if self.state == OPEN:
+            screen.draw_open(self.x, self.y, self.count, self.player_num)
+        elif self.state == CLOSE:
+            screen.draw_close(self.x, self.y)
+        elif self.state == LOCK:
+            screen.draw_lock(self.x, self.y, self.player_num)
+        elif self.state == EXPLODED:
+            screen.draw_bomb(self.x, self.y, self.player_num)
+
+
+class Main:
+    def __init__(self):
+        self.field = []
+        self.turn = 1
+        self.remain_bomb = 0
+        self.point1 = 0
+        self.point2 = 0
+        self.is_first = True
+        self.pass_count = 0
+
+    def initialize(self):
+        screen.reset()
+        screen.set_bind()
+        self.turn = 1
+        self.remain_bomb = BOMB_COUNT
+        self.point1 = 0
+        self.point2 = 0
+        self.is_first = True
+        self.pass_count = 0
+
+        self.field = []
+        for y in range(COLUMN):
+            line = []
+            for x in range(ROW):
+                line.append(Cell(x, y, self.field))
+            self.field.append(line)
+        self.draw()
+
+    def change_turn(self, pass_turn=False):
+        self.miss_check()
+        if not pass_turn:
+            self.check_clear()
+            self.pass_count = 0
+        if self.turn == 1:
+            self.turn = 2
+        elif self.turn == 2:
+            self.turn = 1
+        screen.draw_turn(self.turn)
+
+    def calc_score(self, score, player_num):
+        if player_num == 1:
+            self.point1 += score
+            screen.draw_score(self.turn, self.point1)
+        elif player_num == 2:
+            self.point2 += score
+            screen.draw_score(self.turn, self.point2)
+
+    def set_bombs(self, x0, y0):
+        # 爆弾の位置決め
+        lis = []
+        for y in range(COLUMN):
+            for x in range(ROW):
+                if x0 - 1 <= x <= x0 + 1 and y0 - 1 <= y <= y0 + 1:
+                    # if x == x0 and y == y0:
+                    continue
+                lis.append([x, y])
+        random.shuffle(lis)
+        for i in range(BOMB_COUNT):
+            pos = lis.pop()
+            self.field[pos[1]][pos[0]].set_bomb()
+
+        # 各マス周囲の爆弾の数を数える
+        for y in range(COLUMN):
+            for x in range(ROW):
+                count = 0
+                for b in range(-1, 2):
+                    for a in range(-1, 2):
+                        if a == 0 and b == 0: continue
+                        if out_of_field(x + a, y + b): continue
+                        if self.field[y + b][x + a].is_bomb():
+                            count += 1
+                self.field[y][x].set_count(count)
+
+    def miss_check(self):
+        for y in range(len(self.field)):
+            for x in range(len(self.field[0])):
+                if self.field[y][x].check_lock_miss():
+                    if self.turn == 1:
+                        self.point1 -= MISS_PENALTY
+                        screen.draw_score(self.turn, self.point1)
+                    if self.turn == 2:
+                        self.point2 -= MISS_PENALTY
+                        screen.draw_score(self.turn, self.point2)
+                    screen.draw_text(Log.MISS_LOCK, self.turn, MISS_PENALTY, x, y)
+                    self.field[y][x].lock(0)
+                    self.field[y][x].open(0)
+
+    def check_clear(self):
+        is_clear = True
+        for line in self.field:
+            for cell in line:
+                if (cell.get_state() == CLOSE or cell.get_state() == LOCK) and not cell.is_bomb():
+                    is_clear = False
+        if not is_clear:
+            return
+        if self.point1 > self.point2:
+            screen.draw_text(Log.WIN, 1)
+        elif self.point1 < self.point2:
+            screen.draw_text(Log.WIN, 2)
+        elif self.point1 == self.point2:
+            screen.draw_text(Log.DRAW, 0)
+
+    def lock(self):
+        self.remain_bomb -= 1
+        screen.draw_bomb_count(self.remain_bomb)
+
+    def unlock(self):
+        self.remain_bomb += 1
+        screen.draw_bomb_count(self.remain_bomb)
+
+    def explode(self):
+        self.lock()
+        if self.turn == 1:
+            self.point1 -= EXPLODE_PENALTY
+        if self.turn == 2:
+            self.point2 -= EXPLODE_PENALTY
+        screen.draw_text(Log.EXPLODE, self.turn, EXPLODE_PENALTY)
+
+    def pass_turn(self):
+        self.pass_count += 1
+        screen.draw_text(Log.PASS, self.turn)
+        if self.pass_count < 2 or self.is_first:
+            self.change_turn(True)
+            return
+        screen.draw_text(Log.AUTO_OPEN)
+        self.pass_count = 0
+        lis = []
+        for y in range(COLUMN):
+            for x in range(ROW):
+                if self.field[y][x].get_state() == CLOSE and not self.field[y][x].is_bomb():
+                    is_edge = False
+                    for b in range(-1, 2):
+                        for a in range(-1, 2):
+                            if a == 0 and b == 0: continue
+                            if out_of_field(x + a, y + b): continue
+                            if self.field[y + b][x + a].get_state() == OPEN:
+                                is_edge = True
+                                lis.append([x, y])
+                                break
+                        if is_edge:
+                            break
+        if not lis:
+            for y in range(COLUMN):
+                for x in range(ROW):
+                    if self.field[y][x].get_state() == CLOSE and not self.field[y][x].is_bomb():
+                        lis.append([x, y])
+        random.shuffle(lis)
+        pos = lis.pop()
+        self.field[pos[1]][pos[0]].left_click(0)
+
+    def retry(self):
+        self.initialize()
+
+    def draw(self):
+        for line in self.field:
+            for cell in line:
+                cell.draw()
+
+    def clicked(self, x, y, type):
+        if type == LEFT:
+            if self.is_first:
+                self.is_first = False
+                self.set_bombs(x, y)
+                self.field[y][x].left_click(0)
+            self.field[y][x].left_click(self.turn)
+        elif type == RIGHT:
+            self.field[y][x].right_click(self.turn)
+
+
+screen = Screen(ROW, COLUMN, BOMB_COUNT)
+
+main = Main()
+
+screen.set_main(main)
+
+main.initialize()
+
+screen.mainloop()
---
+++ src/Screen.py
@@ -0,0 +1,218 @@
+from tkinter import *
+import tkinter
+from enum import Enum, auto
+
+# ====================================================================================================================
+WIDTH = 800
+HEIGHT = 600
+FIELD_WIDTH = 400
+FIELD_HEIGHT = 400
+# 盤面左上角の座標
+FIELD_X = 40
+FIELD_Y = 40
+
+LEFT = 0
+RIGHT = 1
+
+# ====================================================================================================================
+
+tk = Tk()
+canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg="lightgreen")
+canvas.pack()
+
+
+class Log(Enum):
+    DELETE = auto()
+    EXPLODE = auto()
+    MISS_LOCK = auto()
+    WIN = auto()
+    DRAW = auto()
+    PASS = auto()
+    AUTO_OPEN = auto()
+
+
+# 描画用のクラス
+class Screen:
+    turn_label: tkinter.Label
+    bomb_label: tkinter.Label
+    player1_label: tkinter.Label
+    player2_label: tkinter.Label
+
+    pass_button: tkinter.Button
+    retry_button: tkinter.Button
+    exit_button: tkinter.Button
+    log_frame: tkinter.Frame
+    log_text: tkinter.Text
+    log_bar: tkinter.Scrollbar
+
+    def __init__(self, row, column, bomb_count):
+        self.row = row
+        self.column = column
+        self.max_bomb = bomb_count
+        self.cell_size = min(FIELD_WIDTH // row, FIELD_HEIGHT // column)
+
+        for i in range(self.column):
+            canvas.create_text(60 + 40 * i, 20, text=chr(ord('a') + i), font=('Times', 24))
+        for i in range(self.row):
+            canvas.create_text(20, 60 + 40 * i, text=i + 1, font=('Times', 24))
+
+        canvas.create_rectangle(20, 460, 440, 580, fill="white")
+        canvas.create_rectangle(460, 20, 780, 440, fill="white")
+        canvas.create_rectangle(460, 460, 780, 580, fill="white")
+        canvas.create_text(626, 44, text="マルチスイーパー", font=('Times', 28))
+        canvas.create_text(515, 110, text="現在：", font=('Times', 20))
+        self.turn_label = tkinter.Label(text="Player1", font=('Times', 20), bg="white", fg="red")
+        self.turn_label.place(x=555, y=90)
+        canvas.create_text(515, 185, text="爆弾：", font=('Times', 20))
+        self.bomb_label = tkinter.Label(text=f"{self.max_bomb}/{self.max_bomb}", font=('Times', 20), bg="white")
+        self.bomb_label.place(x=555, y=165)
+        canvas.create_text(120, 500, text="Player1", font=('Times', 20), fill="red")
+        self.player1_label = tkinter.Label(text="0", font=('Times', 20), bg="white")
+        self.player1_label.place(x=100, y=520)
+        canvas.create_text(320, 500, text="Player2", font=('Times', 20), fill="blue")
+        self.player2_label = tkinter.Label(text="0", font=('Times', 20), bg="white")
+        self.player2_label.place(x=300, y=520)
+        canvas.create_rectangle(60, 480, 180, 560)
+
+        self.log_frame = tkinter.Frame(tk)
+        self.log_frame.place(x=480, y=210)
+        self.log_text = tkinter.Text(self.log_frame, width=19, height=4, font=('Times', 20), relief=tkinter.SOLID)
+        self.log_text.grid(row=0, column=0)
+        self.log_bar = tkinter.Scrollbar(self.log_frame, orient=tkinter.VERTICAL)
+        self.log_bar.grid(row=0, column=1, sticky=tkinter.N + tkinter.S)
+        self.log_bar.config(command=self.log_text.yview)
+        self.log_text.config(yscrollcommand=self.log_bar.set)
+
+    def reset(self):
+        self.draw_turn(1)
+        self.bomb_label["text"] = f"{self.max_bomb}/{self.max_bomb}"
+        self.player1_label["text"] = "0"
+        self.player2_label["text"] = "0"
+        self.log_text.delete("1.0", "end")
+
+    def set_main(self, main):
+        self.main = main
+        self.pass_button = tkinter.Button(text="パス", font=('Times', 20), width=10, height=2,
+                                          command=self.main.pass_turn)
+        self.pass_button.place(x=545, y=350)
+        self.retry_button = tkinter.Button(text="やり直す", font=('Times', 20), width=8, height=1, command=self.main.retry)
+        self.retry_button.place(x=480, y=500)
+        self.exit_button = tkinter.Button(text="終了", font=('Times', 20), width=8, height=1, command=tk.destroy)
+        self.exit_button.place(x=630, y=500)
+
+    def draw_turn(self, player_num):
+        if player_num == 1:
+            canvas.create_rectangle(60, 480, 180, 560)
+            canvas.create_rectangle(260, 480, 380, 560, outline="white")
+            self.turn_label["fg"] = "red"
+        elif player_num == 2:
+            canvas.create_rectangle(60, 480, 180, 560, outline="white")
+            canvas.create_rectangle(260, 480, 380, 560)
+            self.turn_label["fg"] = "blue"
+        self.turn_label["text"] = f"Player{player_num}"
+
+    def draw_bomb_count(self, count):
+        self.bomb_label["text"] = f"{count}/{self.max_bomb}"
+
+    def draw_text(self, kind, player_num=-1, penalty=-1, x=-1, y=-1):
+        if kind == Log.EXPLODE:
+            self.log_text.insert("end", f"爆発！\nPlayer{player_num} -{penalty}P!\n")
+        elif kind == Log.MISS_LOCK:
+            pos = str(y + 1) + chr(ord('a') + x)
+            self.log_text.insert("end", f"ミスロック！\nPlayer{player_num} -{penalty}P!\n場所：{pos}\n")
+        elif kind == Log.WIN:
+            self.log_text.insert("end", f"Player{player_num}の勝利！\n")
+        elif kind == Log.DRAW:
+            self.log_text.insert("end", f"引き分け！\n")
+        elif kind == Log.PASS:
+            self.log_text.insert("end", f"Player{player_num}がパス\n")
+        elif kind == Log.AUTO_OPEN:
+            self.log_text.insert("end", f"2連続パス\n1マス開けます\n")
+        self.log_text.yview_moveto(1.0)
+
+    def draw_score(self, player_num, score):
+        if player_num == 1:
+            self.player1_label["text"] = score
+        if player_num == 2:
+            self.player2_label["text"] = score
+
+    def draw_close(self, x, y):
+        canvas.create_rectangle(FIELD_X + x * self.cell_size, FIELD_Y + y * self.cell_size,
+                                FIELD_X + (x + 1) * self.cell_size, FIELD_Y + (y + 1) * self.cell_size,
+                                fill="green")
+
+    def draw_lock(self, x, y, player_num):
+        if player_num == 1:
+            color = "red"
+        else:
+            color = "blue"
+        canvas.create_rectangle(FIELD_X + x * self.cell_size, FIELD_Y + y * self.cell_size,
+                                FIELD_X + (x + 1) * self.cell_size, FIELD_Y + (y + 1) * self.cell_size,
+                                fill="green")
+        canvas.create_polygon(FIELD_X + (x + 0.4) * self.cell_size, FIELD_Y + (y + 0.2) * self.cell_size,
+                              FIELD_X + (x + 0.8) * self.cell_size, FIELD_Y + (y + 0.4) * self.cell_size,
+                              FIELD_X + (x + 0.4) * self.cell_size, FIELD_Y + (y + 0.6) * self.cell_size,
+                              fill=color)
+        canvas.create_line(FIELD_X + (x + 0.4) * self.cell_size, FIELD_Y + (y + 0.2) * self.cell_size,
+                           FIELD_X + (x + 0.4) * self.cell_size, FIELD_Y + (y + 0.9) * self.cell_size,
+                           width=2)
+
+    def draw_miss(self, x, y):
+        canvas.create_line(FIELD_X + x * self.cell_size, FIELD_Y + y * self.cell_size,
+                           FIELD_X + (x + 1) * self.cell_size, FIELD_Y + (y + 1) * self.cell_size)
+        canvas.create_line(FIELD_X + x * self.cell_size, FIELD_Y + (y + 1) * self.cell_size,
+                           FIELD_X + (x + 1) * self.cell_size, FIELD_Y + y * self.cell_size)
+
+    def draw_open(self, x, y, count, player_num):
+        if player_num == 1:
+            color = "mistyrose"
+        elif player_num == 2:
+            color = "lightcyan"
+        else:
+            color = "white"
+        canvas.create_rectangle(FIELD_X + x * self.cell_size, FIELD_Y + y * self.cell_size,
+                                FIELD_X + (x + 1) * self.cell_size, FIELD_Y + (y + 1) * self.cell_size,
+                                fill=color)
+        canvas.create_text(FIELD_X + (x + 0.5) * self.cell_size, FIELD_Y + (y + 0.5) * self.cell_size,
+                           text=f"{count}", font=('Times', 40))
+
+    def draw_bomb(self, x, y, player_num):
+        if player_num == 1:
+            color = "mistyrose"
+        else:
+            color = "lightcyan"
+        canvas.create_rectangle(FIELD_X + x * self.cell_size, FIELD_Y + y * self.cell_size,
+                                FIELD_X + (x + 1) * self.cell_size, FIELD_Y + (y + 1) * self.cell_size,
+                                fill=color)
+        canvas.create_oval(FIELD_X + (x + 0.2) * self.cell_size, FIELD_Y + (y + 0.2) * self.cell_size,
+                           FIELD_X + (x + 0.8) * self.cell_size, FIELD_Y + (y + 0.8) * self.cell_size,
+                           fill="black")
+        canvas.create_line(FIELD_X + (x + 0.2) * self.cell_size, FIELD_Y + (y + 0.2) * self.cell_size,
+                           FIELD_X + (x + 0.8) * self.cell_size, FIELD_Y + (y + 0.8) * self.cell_size, width=3)
+        canvas.create_line(FIELD_X + (x + 0.2) * self.cell_size, FIELD_Y + (y + 0.8) * self.cell_size,
+                           FIELD_X + (x + 0.8) * self.cell_size, FIELD_Y + (y + 0.2) * self.cell_size, width=3)
+        canvas.create_line(FIELD_X + (x + 0.5) * self.cell_size, FIELD_Y + (y + 0.1) * self.cell_size,
+                           FIELD_X + (x + 0.5) * self.cell_size, FIELD_Y + (y + 0.9) * self.cell_size, width=3)
+        canvas.create_line(FIELD_X + (x + 0.1) * self.cell_size, FIELD_Y + (y + 0.5) * self.cell_size,
+                           FIELD_X + (x + 0.9) * self.cell_size, FIELD_Y + (y + 0.5) * self.cell_size, width=3)
+
+    def set_bind(self):
+        canvas.bind("<Button-1>", self.left_clicked)
+        canvas.bind("<Button-3>", self.right_clicked)
+
+    def left_clicked(self, event):
+        x = (event.x - FIELD_X) // self.cell_size
+        y = (event.y - FIELD_Y) // self.cell_size
+        if x < 0 or self.row <= x or y < 0 or self.column <= y:
+            return
+        self.main.clicked(x, y, LEFT)
+
+    def right_clicked(self, event):
+        x = (event.x - FIELD_X) // self.cell_size
+        y = (event.y - FIELD_Y) // self.cell_size
+        if x < 0 or self.row <= x or y < 0 or self.column <= y:
+            return
+        self.main.clicked(x, y, RIGHT)
+
+    def mainloop(self):
+        tk.mainloop()
